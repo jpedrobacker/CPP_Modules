373acf20e605908c53bcfcfe3ef84881e40bcbf6
jpedrobacker <jpedrobacker@outlook.com>
2024-10-01 19:04:12 -0300

    phonebook main classes added

--- CPP_00/ex01/phonebook.cpp
+++ CPP_00/ex01/phonebook.cpp
@@ -6,22 +6,38 @@
 /*   By: jbergfel <jbergfel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 09:55:00 by jbergfel          #+#    #+#             */
-/*   Updated: 2024/09/30 15:41:39 by jbergfel         ###   ########.fr       */
+/*   Updated: 2024/10/01 18:50:36 by jbergfel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 
-void	phonebook::myMethod()
+void	PhoneBook::add_num()
 {
-	std::cout << "TESTE5" << std::endl;
+	std::cout << "TEST ADD" << std::endl ;
+}
+
+void	PhoneBook::search_num()
+{
+	std::cout << "TEST SEARCH" << std::endl;
 }
 
 int main(void)
 {
-	phonebook	book;
+	PhoneBook	book;
+	std::string	input;
 
-	book.myMethod();
+	while (1)
+	{
+		std::cout << "What to do?: ";
+		std::cin >> input;
 
+		if (input == "EXIT")
+			break;
+		else if (input == "ADD")
+			book.add_num();
+		else if (input == "SEARCH")
+			book.search_num();
+	}
 	return (0);
 }
--- CPP_00/ex01/phonebook.hpp
+++ CPP_00/ex01/phonebook.hpp
@@ -6,14 +6,33 @@
 /*   By: jbergfel <jbergfel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 09:55:03 by jbergfel          #+#    #+#             */
-/*   Updated: 2024/09/30 15:39:29 by jbergfel         ###   ########.fr       */
+/*   Updated: 2024/10/01 18:57:56 by jbergfel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <iostream>
 
-class phonebook
+class Contact
 {
+	private:
+		int			index;
+		std::string	name;
+		std::string	last_name;
+		std::string	nickname;
+		std::string	phone_num;
+		std::string	darkest_secret;
 	public:
-		void	myMethod();
+		//adds the rest of methods and general info
+
+} ;
+
+class PhoneBook
+{
+	private:
+		//all contacts should be private
+		Contact		contact[8];
+		int			index;
+	public:
+		void		add_num();
+		void		search_num();
 } ;
