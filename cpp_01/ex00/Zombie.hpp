d7c8475bad27de2ae5597a04eafa90ad328a8225
jbergfel <jpedrobacker@outlook.com>
2024-10-14 11:42:19 -0300

    cpp01 ex01 started

---
+++ cpp_01/ex00/Zombie.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbergfel <jbergfel@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/14 11:09:39 by jbergfel          #+#    #+#             */
+/*   Updated: 2024/10/14 11:18:44 by jbergfel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+
+class Zombie
+{
+	private:
+		std::string _zombieName;
+
+	public:
+		Zombie();
+		~Zombie();
+};
+
+#endif
