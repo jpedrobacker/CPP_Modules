3ddde8229427c538ab95c81ab8fdf767c13d1555
jpedrobacker <jpedrobacker@outlook.com>
2025-06-09 13:25:10 -0300

    cpp_07 ex01 done

--- cpp_07/ex01/Iter.hpp
+++ cpp_07/ex01/Iter.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   iter.hpp                                           :+:      :+:    :+:   */
+/*   Iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jbergfel <jbergfel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 12:48:30 by jbergfel          #+#    #+#             */
-/*   Updated: 2025/06/02 12:49:18 by jbergfel         ###   ########.fr       */
+/*   Updated: 2025/06/09 13:00:46 by jbergfel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,11 @@
 
 # include <iostream>
 # include <string>
+
+template <typename T> void iter(T* arr, int len, void (*f)(T&))
+{
+	for (int i = 0; i < len; ++i)
+		f(arr[i]);
+}
 
 #endif
--- cpp_07/ex01/main.cpp
+++ cpp_07/ex01/main.cpp
@@ -6,8 +6,34 @@
 /*   By: jbergfel <jbergfel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 12:48:34 by jbergfel          #+#    #+#             */
-/*   Updated: 2025/06/02 15:31:19 by jbergfel         ###   ########.fr       */
+/*   Updated: 2025/06/09 13:24:13 by jbergfel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Iter.hpp"
+
+void	increment(int &x)
+{
+	x++;
+}
+
+void	to_upper_char(char &c)
+{
+	if (c >= 'a' && c <='z')
+		c -= 32;
+}
+
+int main()
+{
+	int intArr[] = {1, 2, 3, 4, 5};
+	char charArr[] = {'a', 'b', 'c', 'd', 'e'};
+
+	iter(intArr, 5, increment);
+	iter(charArr, 3, to_upper_char);
+	for (int x = 0; x < 5; x++)
+	{
+		std::cout << "post iter int x: " << intArr[x] << std::endl;
+		std::cout << "post iter char x: " << charArr[x] << std::endl;
+	}
+	return (0);
+}
