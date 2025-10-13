235e1e538b581ed7543716d3c184444a089f585e
jbergfel <jpedrobacker@outlook.com>
2025-10-13 17:48:06 -0300

    last push cpp06

--- cpp_06/ex02/main.cpp
+++ cpp_06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jbergfel <jbergfel@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 17:49:31 by jbergfel          #+#    #+#             */
-/*   Updated: 2025/05/29 13:07:03 by jbergfel         ###   ########.fr       */
+/*   Updated: 2025/08/27 14:48:55 by jbergfel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ int main()
 {
 	Base *b;
 	b = generate();
-	std::cout << "pointer" << std::endl;
+	std::cout << "pointer:" << std::endl;
 	identify(b);
+	std::cout << "address:" << std::endl;
+	identify(*b);
 
 	delete b;
 	return (0);
